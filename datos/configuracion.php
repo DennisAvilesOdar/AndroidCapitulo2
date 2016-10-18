6e7b1b27f5a4db4c89e4016c03f2083629b6b4fb
Dennis Aviles <dennis.aviles@icloud.com>
2016-10-18 13:23:26 -0500

    comienso de proyecto para android

---
+++ datos/configuracion.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+define("SERVIDOR_BD", "ec2-174-129-18-170.compute-1.amazonaws.com");
+define("USUARIO_BD", "neyclrgqcpcvqv");
+define("CLAVE_BD", "fMrppHQOf72-s-mPCpi3YEWEDa");
+define("PUERTO_BD", "5432");
+define("NOMBRE_BD", "d6fejas54o8sb6");
+ 
+*/
+
+define("SERVIDOR_BD", "localhost");
+define("USUARIO_BD", "postgres");
+define("CLAVE_BD", "USAT2016");
+define("PUERTO_BD", "5432");
+define("NOMBRE_BD", "bdcomercial_ws");
