423226a768f41f27fee4c846a82f7f6adf19a5b3
Dennis Aviles <dennis.aviles@icloud.com>
2016-10-27 15:41:27 -0500

    nuevos documentos

---
+++ webservice/articulo.listar.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once '../negocio/articulo.php';
+require_once '../util/funciones/Funciones.clase.php';
+require_once 'token.validar.php';
+if (! isset($_POST["token"])){
+    Funciones::imprimeJSON(500, "debe especificar un token", "");
+    exit();
+}
+
+$email = $_POST["token"];
+
+
+try {
+    
+    if (validarToken($token)) {
+        $obj = new articulo();
+        $resultado = $obj->lis
+    }
+    
+} catch (Exception $exc) {
+    
+    Funciones::imprimeJSON(500, $exc->getMessage(), "");
+}
