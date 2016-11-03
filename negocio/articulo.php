de495f25a19a3e628b487d675b56b89d6f9786fa
Dennis Aviles <dennis.aviles@icloud.com>
2016-11-03 14:06:11 -0500

    modificacion web service

--- negocio/articulo.php
+++ negocio/articulo.php
@@ -16,4 +16,26 @@ class Articulo extends Conexion {
         }
     }
     
+    public function obtenerFoto($codigoArticulo){
+        $foto = "../imagenes/".$codigoArticulo;
+        
+        if (file_exists($foto . ".png")) {
+            $foto = $foto . ".png";
+        }else if (file_exists($foto . ".PNG")){
+            $foto = $foto . ".PNG";
+        }else if (file_exists($foto . ".jpg")){
+            $foto = $foto . ".jpg";
+        }else if (file_exists($foto . ".JPG")){
+            $foto = $foto . ".JPG";
+        }else{
+            $foto = "none";
+        }
+        
+        if ($foto == "none") {
+            return $foto;
+        }else{
+            return Funciones::$DIRECCION_WEB_SERVICE . $foto;
+        }
+    }
+    
 }
--- webservice/articulo.listar.php
+++ webservice/articulo.listar.php
@@ -16,6 +16,25 @@ try {
     if (validarToken($token)) {
         $obj = new articulo();
         $resultado = $obj->listar();
+        
+        $listaArticulo = array();
+        for ($i=0; $i<count($resultado);$i++){
+            
+            /*Obtener foto articulo*/
+            $foto = $obj->obtenerFoto($resultado[$i]["codigo_articulo"]);
+            /*Obtener foto articulo*/
+            
+            $datosArticulo = array
+                    (
+                        "codigo" => $resultado[$i]["codigo_articulo"],
+                        "nombre" => $resultado[$i]["nombre"],
+                        "precio" => $resultado[$i]["precio_venta"],
+                        "stock" => $resultado[$i]["stock"],
+                        "foto" => $foto
+                    );
+            $listaArticulo[$i] = $datosArticulo;
+        }
+        
         Funciones::imprimeJSON(200,"",$resultado);
         
     }
