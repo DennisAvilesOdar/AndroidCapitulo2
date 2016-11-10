d3e030781e5a88f5736c65c727f15f790d54a24f
Dennis Aviles <dennis.aviles@icloud.com>
2016-11-10 15:36:50 -0500

    .

--- webservice/cliente_listar.php
+++ webservice/cliente_listar.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../negocio/articulo.php';
+require_once '../negocio/cliente.clase.php';
 require_once '../util/funciones/Funciones.clase.php';
 require_once 'token.validar.php';
 if (! isset($_POST["token"])){
@@ -14,28 +14,10 @@ $token = $_POST["token"];
 try {
     
     if (validarToken($token)) {
-        $obj = new articulo();
+        $obj = new cliente();
         $resultado = $obj->listar();
         
-        $listaArticulo = array();
-        for ($i=0; $i<count($resultado);$i++){
-            
-            /*Obtener foto articulo*/
-            $foto = $obj->obtenerFoto($resultado[$i]["codigo_articulo"]);
-            /*Obtener foto articulo*/
-            
-            $datosArticulo = array
-                    (
-                        "codigo" => $resultado[$i]["codigo_articulo"],
-                        "nombre" => $resultado[$i]["nombre"],
-                        "precio" => $resultado[$i]["precio_venta"],
-                        "stock" => $resultado[$i]["stock"],
-                        "foto" => $foto
-                    );
-            $listaArticulo[$i] = $datosArticulo;
-        }
-        
-        Funciones::imprimeJSON(200,"",$listaArticulo);
+        Funciones::imprimeJSON(200,"",$resultado);
         
     }
     
